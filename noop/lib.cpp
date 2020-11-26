2264b3067f91d100532b397d360fd33dcb42e9bf
Tim Graupmann <tgraupmann@gmail.com>
2020-11-26 12:02:55 -0800

    Added noop library

---
+++ noop/lib.cpp
@@ -0,0 +1,13 @@
+// lib.cpp : Send data to a WSS WebSocket.
+//
+
+#include "lib.h"
+#include <iostream>
+
+using namespace std;
+
+
+void DoNoop()
+{
+    cout << "Doing NOOP" << endl;
+}
---
+++ noop/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp : Send data to a WSS WebSocket.
+//
+
+#include "lib.h"
+
+int main()
+{
+    DoNoop();
+}
